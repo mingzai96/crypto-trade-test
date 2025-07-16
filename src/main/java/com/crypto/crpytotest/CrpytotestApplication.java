6a41b584dd2853aeea99ae9375a090f4f6d5ec42
Ming Zai <cmsheh@hotmail.com>
2025-07-16 14:07:20 +0800

    Initial Commit

---
+++ src/main/java/com/crypto/crpytotest/CrpytotestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crypto.crpytotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrpytotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrpytotestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crypto/crpytotest/CrpytotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crypto.crpytotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrpytotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
